77ae501f3c99d5d9e172430b5c536e0262ddacb0
JORGE AGUILAR GUTIERREZ <94917439+Jorgeaguilar04@users.noreply.github.com>
2022-11-14 13:48:29 -0300

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+<h1>El listado se muestra en la siguiente página</h1>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+
+
+</script>
+
+
---
+++ src/views/AboutView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+ <ul>
+ <li v-for="(Typeservice,i) in Typeservice" :key="i"></li>
+ <h1> {{Typeservice}}</h1>
+ </ul>
+  </div>
+</template>
+
+<script>
+import Typeservice from "../services/api"
+export default{
+name: "Home",
+data(){
+return{
+Typeservice: []
+}
+},
+methods: {
+ async getTypeservice () {
+ const response= await Typeservice.getTypeservice()
+ this.Typeservice = response.data
+ }
+ },
+ created () {
+ this.getTypeservice()
+ }
+ }
+</script>
